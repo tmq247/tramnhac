376b799ea667ab7c34f67fba37b4e56fa60facce
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-10-13 19:09:03 +0700

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -53,7 +53,7 @@ async def play_commnd(
     url,
     fplay,
 ):
-    file_id = CgACAgUAAx0CfCNV-QACUulnC7NOaKzSFV6WVXI1QaBX6wABeYoAAhwSAAIyp1hUF3MW_fxrm_oeBA
+    file_id = "CgACAgUAAx0CfCNV-QACUulnC7NOaKzSFV6WVXI1QaBX6wABeYoAAhwSAAIyp1hUF3MW_fxrm_oeBA"
     mystic = await message.reply_animation(
         animation=file_id,
         caption=_["play_2"].format(channel) if channel else _["play_1"]
