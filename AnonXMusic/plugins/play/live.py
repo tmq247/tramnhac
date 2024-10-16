af71f6c8f044c8cebec800d433074e75116c4ed0
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-10-16 20:16:27 +0700

    Update live.py

--- AnonXMusic/plugins/play/live.py
+++ AnonXMusic/plugins/play/live.py
@@ -32,7 +32,8 @@ async def play_live_stream(client, CallbackQuery, _):
         
     file_id = "CgACAgUAAx0CfCNV-QACUulnC7NOaKzSFV6WVXI1QaBX6wABeYoAAhwSAAIyp1hUF3MW_fxrm_oeBA"
     mystic = await CallbackQuery.message.reply_animation(animation=file_id,
-        _["play_2"].format(channel) if channel else _["play_1"])
+        _["play_2"].format(channel) if channel else _["play_1"]
+                                                        )
     try:
         details, track_id = await YouTube.track(vidid, True)
     except:
