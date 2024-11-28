00359d9cef933da7eb84b45d3391aed187ab21c8
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-11-28 09:24:38 +0700

    Update extraction.py

--- AnonXMusic/utils/extraction.py
+++ AnonXMusic/utils/extraction.py
@@ -1,20 +1,10 @@
+
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
-import pyrogram.utils as utils
+
 from AnonXMusic import app
 
-def get_peer_type(peer_id: int) -> str:
-    print('get_peer_type call')
-    peer_id_str = str(peer_id)
-    if not peer_id_str.startswith("-"):
-        return "user"
-    elif peer_id_str.startswith("-100"):
-        return "channel"
-    else:
-        return "chat"
 
-
-utils.get_peer_type = get_peer_type
 async def extract_user(m: Message) -> User:
     if m.reply_to_message:
         return m.reply_to_message.from_user
